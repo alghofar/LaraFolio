08b88d5c5b09c01d14f21aec4cae73acc8d9a9bf
Julien Bonvarlet <jbonva@gmail.com>
2014-03-03 18:03:11 +0100

    Fixed typo

--- app/Tyloo/Controllers/BaseController.php
+++ app/Tyloo/Controllers/BaseController.php
@@ -7,79 +7,79 @@ use Illuminate\Support\Facades\Redirect;
 
 class BaseController extends Controller
 {
-    /**
-     * The layout used by the controller.
-     *
-     * @var \Illuminate\View\View
-     */
-    protected $layout = 'layouts.main';
+	/**
+	 * The layout used by the controller.
+	 *
+	 * @var \Illuminate\View\View
+	 */
+	protected $layout = 'layouts.main';
 
-    /**
-     * Create a new BaseController instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->beforeFilter('csrf', [ 'on' => 'post' ]);
-    }
+	/**
+	 * Create a new BaseController instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->beforeFilter('csrf', ['on' => 'post']);
+	}
 
-    /**
-     * Setup the layout used by the controller.
-     *
-     * @return void
-     */
-    protected function setupLayout()
-    {
-        if (! is_null($this->layout)) {
-            $this->layout = View::make($this->layout);
-        }
-    }
+	/**
+	 * Setup the layout used by the controller.
+	 *
+	 * @return void
+	 */
+	protected function setupLayout()
+	{
+		if (! is_null($this->layout)) {
+			$this->layout = View::make($this->layout);
+		}
+	}
 
-    /**
-     * Set the specified view as content on the layout.
-     *
-     * @param  string  $path
-     * @param  array  $data
-     * @return void
-     */
-    protected function view($path, $data = [])
-    {
-        $this->layout->content = View::make($path, $data);
-    }
+	/**
+	 * Set the specified view as content on the layout.
+	 *
+	 * @param  string  $path
+	 * @param  array  $data
+	 * @return void
+	 */
+	protected function view($path, $data = [])
+	{
+		$this->layout->content = View::make($path, $data);
+	}
 
-    /**
-     * Redirect to the specified named route.
-     *
-     * @param  string  $route
-     * @param  array  $params
-     * @param  array  $data
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    protected function redirectRoute($route, $params = [], $data = [])
-    {
-        return Redirect::route($route, $params)->with($data);
-    }
+	/**
+	 * Redirect to the specified named route.
+	 *
+	 * @param  string  $route
+	 * @param  array  $params
+	 * @param  array  $data
+	 * @return \Illuminate\Http\RedirectResponse
+	 */
+	protected function redirectRoute($route, $params = [], $data = [])
+	{
+		return Redirect::route($route, $params)->with($data);
+	}
 
-    /**
-     * Redirect back with old input and the specified data.
-     *
-     * @param  array $data
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    protected function redirectBack($data = [])
-    {
-        return Redirect::back()->withInput()->with($data);
-    }
+	/**
+	 * Redirect back with old input and the specified data.
+	 *
+	 * @param  array $data
+	 * @return \Illuminate\Http\RedirectResponse
+	 */
+	protected function redirectBack($data = [])
+	{
+		return Redirect::back()->withInput()->with($data);
+	}
 
-    /**
-     * Redirect a logged in user to the previously intended url.
-     *
-     * @param  mixed $default
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    protected function redirectIntended($default = null, $data = [])
-    {
-        return Redirect::intended($default)->with($data);
-    }
+	/**
+	 * Redirect a logged in user to the previously intended url.
+	 *
+	 * @param  mixed $default
+	 * @return \Illuminate\Http\RedirectResponse
+	 */
+	protected function redirectIntended($default = null, $data = [])
+	{
+		return Redirect::intended($default)->with($data);
+	}
 }
